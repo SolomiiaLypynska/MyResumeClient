90635b94782fbd8dcbba4fa952bf1a64a33ea548
Solomiia Lypynska <yaremkosolomiya@gmail.com>
2023-05-03 22:19:35 +0300

    add avatar in profile component

--- src/component/profile/Profile.jsx
+++ src/component/profile/Profile.jsx
@@ -1,4 +1,5 @@
-import { Breadcrumb, Layout, theme } from 'antd';
+import { UserOutlined } from '@ant-design/icons';
+import { Avatar, Breadcrumb, Col, Layout, Row, Space, theme } from 'antd';
 import React, { useEffect, useState } from "react";
 import { getUser } from "../../service/UserService";
 
@@ -26,8 +27,13 @@ export const Profile = () => {
                 <Content style={{ margin: '0 16px' }}>
                     <Breadcrumb items={[{ title: 'User' },
                     { title: <a href="">{user.firstName}</a> }]} />
-                    <div style={{ padding: 24, minHeight: 360, background: colorBgContainer }}>
-                        USER ACCOUNT
+                    <div style={{ padding: 24, minHeight: 700, background: colorBgContainer }}>
+                        <Row justify={'space-between'} gutter={[24]}>
+                            <Col span={3}> <Space wrap size={16}> <Avatar size={80} icon={<UserOutlined />} /> </Space> </Col>
+                        </Row>
+                        <Row justify={'space-between'} gutter={[24]} >
+                            <Col span={3}> <Space wrap size={16}><b>{`${user.firstName} ${user.lastName}`}</b></Space> </Col>
+                        </Row>
                     </div>
                 </Content>
                 <Footer style={{ textAlign: 'center' }}>My-Resume ©2023 Created by S.L.</Footer>
