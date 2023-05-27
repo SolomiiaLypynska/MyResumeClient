398e8390f38745bd6e7db43ba2fe5e9f0a87dde1
Solomiia Lypynska <yaremkosolomiya@gmail.com>
2023-05-27 23:58:06 +0300

    refactoring

--- src/component/profile/workExperience/WorkExperience.jsx
+++ src/component/profile/workExperience/WorkExperience.jsx
@@ -14,22 +14,25 @@ export const WorkExperience = ({ profileUser, getProfileUser }) => {
 //TODO add List from antd
 //TODO add Popconfirm for delete antd
     return (<>
-        <Row className='experience-title' justify={'space-between'} gutter={[24]} ><Col span={3}> 
-        <Title level={4}>Experience</Title> </Col>
-            <Col span={1}> <Button icon={<PlusOutlined className='experience-button' />} type="text" onClick={showModal} /> </Col>
+        <Row className='experience-title' justify={'space-between'} gutter={[24]} > 
+        <Title level={4}>Experience</Title> 
+        <Button icon={<PlusOutlined className='experience-button' />} type="text" onClick={showModal} />
             {/* <Col span={1}> <Button icon={<EditOutlined className='experience-button' />} type="text" /> </Col> */}
         </Row>
 
         {profileUser?.workExperiences?.map((profile, index) => <div key={profile.workExperienceId} className='experience-block'>
             <Row key={"position"} justify={'space-between'} gutter={[24]} align="top">
-                <Col span={24} > 
-                <Divider orientation="left">{profile.positionTitle} {profile.companyName}</Divider> </Col>
+                <Col span={22} > 
+                <Divider orientation="left">{profile.positionTitle} </Divider> </Col>
             </Row>
             <Row key={"employmentType"} gutter={[24]} >
-                <Col span={20} offset={1}> <Space >{profile.employmentType}</Space> </Col>
+                <Col  > <Space >{profile.companyName}</Space> </Col>
+            </Row>
+            <Row key={"employmentType"} gutter={[24]} >
+                <Col > <Space >{profile.employmentType}</Space> </Col>
             </Row>
             <Row key={"employmentDate"} gutter={[24]} >
-                <Col span={22} offset={1}> <Space wrap size={16}>{profile.startDate} - {profile.endDate ? profile.endDate : "Present"}
+                <Col  > <Space wrap size={16}>{profile.startDate} - {profile.endDate ? profile.endDate : "Present"}
                 </Space> </Col>
             </Row>
         </div>
